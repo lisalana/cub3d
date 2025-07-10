17ab56b79ea5b3a67d78b22c775bfbe2d5b5aef6
lisalana <chaneliza@live.fr>
2025-07-10 23:15:22 +0200

    delete main.c

--- main.c
+++ main.c
@@ -1,18 +1,1 @@
-#include "cub3d.h"
 
-int	main(int ac, char **av)
-{
-	t_data	data;
-	if (ac != 2)
-	{
-		printf("usage: %s <map.cub>\n", av[0]);
-		return (1);
-	}
-	if (parse_file(av[1], &data))
-	{
-		printf("Erreur pendant le parsing\n");
-		return (1);
-	}
-	printf("Parsing termine\n");
-	return (0);
-}
