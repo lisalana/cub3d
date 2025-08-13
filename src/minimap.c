bb2c7ecd2fcbc8df9d86b03528819320d8e42384
Lisa <chaneliza@live.fr>
2025-08-13 02:40:00 +0200

    Add files via upload

--- src/minimap.c
+++ src/minimap.c
@@ -6,197 +6,117 @@
 /*   By: reeer-aa <reeer-aa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/25 13:31:05 by reeer-aa          #+#    #+#             */
-/*   Updated: 2025/07/25 13:47:59 by reeer-aa         ###   ########.fr       */
+/*   Updated: 2025/08/11 12:21:39 by reeer-aa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-static void	draw_minimap_inner_background(t_data *game, int start_x,
-		int start_y)
+static void	draw_single_fov_line(t_data *game, t_point center, int direction)
 {
-	int x, y;
-	y = start_y;
-	while (y < start_y + MINIMAP_SIZE)
+	double	angle;
+	int		fov_length;
+	t_point	end;
+
+	angle = calculate_fov_angle(game, direction);
+	fov_length = MINIMAP_TILE_SIZE * 3;
+	end.x = center.x + (int)(cos(angle) * fov_length);
+	end.y = center.y + (int)(sin(angle) * fov_length);
+	draw_line(game, center, end, COLOR_FOV_LINE);
+}
+
+static void	draw_fov_lines(t_data *game, int center_x, int center_y)
+{
+	t_point	center;
+	int		i;
+
+	center.x = center_x;
+	center.y = center_y;
+	i = -1;
+	while (i <= 1)
 	{
-		x = start_x;
-		while (x < start_x + MINIMAP_SIZE)
-		{
-			if (x >= 0 && x < WINDOW_WIDTH && y >= 0 && y < WINDOW_HEIGHT)
-				put_pixel(game, x, y, 0x404040);
-			x++;
-		}
-		y++;
+		draw_single_fov_line(game, center, i);
+		i += 2;
 	}
 }
 
-static void	draw_minimap_background(t_data *game)
+static void draw_player_direction(t_data *game, int center_x, int center_y)
 {
-	int	start_x;
-	int	start_y;
-
-	int x, y;
-	start_x = MINIMAP_MARGIN;
-	start_y = WINDOW_HEIGHT - MINIMAP_SIZE - MINIMAP_MARGIN;
-	y = start_y - 2;
-	while (y < start_y + MINIMAP_SIZE + 2)
-	{
-		x = start_x - 2;
-		while (x < start_x + MINIMAP_SIZE + 2)
-		{
-			if (x >= 0 && x < WINDOW_WIDTH && y >= 0 && y < WINDOW_HEIGHT)
-				put_pixel(game, x, y, 0x000000);
-			x++;
-		}
-		y++;
-	}
-	draw_minimap_inner_background(game, start_x, start_y);
+    double dir_length = MINIMAP_PLAYER_SIZE * 1.8;
+    t_point center, dir_end;
+    
+    center.x = center_x;
+    center.y = center_y;
+    
+    dir_end.x = center_x + (int)(game->player.dir[1] * dir_length);
+    dir_end.y = center_y + (int)(-game->player.dir[0] * dir_length);
+    
+    // Dessiner la flèche
+    draw_line(game, center, dir_end, COLOR_DIRECTION);
+    center.x++;
+    dir_end.x++;
+    draw_line(game, center, dir_end, COLOR_DIRECTION);
+    center.x--;
+    center.y++;
+    dir_end.x--;
+    dir_end.y++;
+    draw_line(game, center, dir_end, COLOR_DIRECTION);
 }
 
-static void	draw_minimap_tile(t_data *game, int screen_x, int screen_y,
-		int color)
-{
-	int	pixel_x;
-	int	pixel_y;
 
-	int x, y;
-	y = 0;
-	while (y < MINIMAP_TILE_SIZE)
-	{
-		x = 0;
-		while (x < MINIMAP_TILE_SIZE)
-		{
-			pixel_x = screen_x + x;
-			pixel_y = screen_y + y;
-			if (pixel_x >= MINIMAP_MARGIN && pixel_x < MINIMAP_MARGIN
-				+ MINIMAP_SIZE && pixel_y >= WINDOW_HEIGHT - MINIMAP_SIZE
-				- MINIMAP_MARGIN && pixel_y < WINDOW_HEIGHT - MINIMAP_MARGIN)
-			{
-				put_pixel(game, pixel_x, pixel_y, color);
-			}
-			x++;
-		}
-		y++;
-	}
+static void draw_test_directions(t_data *game, int center_x, int center_y)
+{
+    int length = 20;
+    
+    // Flèche ROUGE vers la droite (Est)
+    draw_line(game, (t_point){center_x, center_y}, 
+              (t_point){center_x + length, center_y}, 0xFF0000);
+    
+    // Flèche VERTE vers le bas (Sud)  
+    draw_line(game, (t_point){center_x, center_y}, 
+              (t_point){center_x, center_y + length}, 0x00FF00);
+    
+    // Flèche BLEUE vers la gauche (Ouest)
+    draw_line(game, (t_point){center_x, center_y}, 
+              (t_point){center_x - length, center_y}, 0x0000FF);
+    
+    // Flèche JAUNE vers le haut (Nord)
+    draw_line(game, (t_point){center_x, center_y}, 
+              (t_point){center_x, center_y - length}, 0xFFFF00);
 }
 
-static void	draw_player_square(t_data *game, int center_x, int center_y)
+static void draw_modern_player(t_data *game)
 {
-	int	pixel_x;
-	int	pixel_y;
+    // static int debug_counter = 0;
+    // debug_counter++;
+    
+    // // Debug toutes les 60 frames
+    // if (debug_counter % 60 == 0)
+    // {
+    //     printf("\n=== DEBUG MINIMAP ===\n");
+    //     printf("Position joueur: (%.1f, %.1f)\n", game->player.pos[0], game->player.pos[1]);
+    //     printf("Direction joueur: (%.2f, %.2f)\n", game->player.dir[0], game->player.dir[1]);
+    //     printf("Position map: (%d, %d)\n",
+    //            (int)(game->player.pos[0] / TILESIZE),
+    //            (int)(game->player.pos[1] / TILESIZE));
+    //     printf("===================\n\n");
+    // }
+    
+    t_point center;
+    center.x = MINIMAP_MARGIN + MINIMAP_SIZE / 2;
+    center.y = WINDOW_HEIGHT - MINIMAP_SIZE - MINIMAP_MARGIN + MINIMAP_SIZE / 2;
+    
+    draw_fov_lines(game, center.x, center.y);
+    draw_circle(game, center, MINIMAP_PLAYER_SIZE / 2 + 1, 0x000000);
+    draw_circle(game, center, MINIMAP_PLAYER_SIZE / 2, COLOR_PLAYER);
+    draw_player_direction(game, center.x, center.y);
 
-	int x, y;
-	y = -MINIMAP_PLAYER_SIZE / 2;
-	while (y <= MINIMAP_PLAYER_SIZE / 2)
-	{
-		x = -MINIMAP_PLAYER_SIZE / 2;
-		while (x <= MINIMAP_PLAYER_SIZE / 2)
-		{
-			pixel_x = center_x + x;
-			pixel_y = center_y + y;
-			if (pixel_x >= MINIMAP_MARGIN && pixel_x < MINIMAP_MARGIN
-				+ MINIMAP_SIZE && pixel_y >= WINDOW_HEIGHT - MINIMAP_SIZE
-				- MINIMAP_MARGIN && pixel_y < WINDOW_HEIGHT - MINIMAP_MARGIN)
-			{
-				put_pixel(game, pixel_x, pixel_y, 0xFF0000);
-			}
-			x++;
-		}
-		y++;
-	}
-}
-
-static void	draw_minimap_player(t_data *game)
-{
-	int		center_x;
-	int		center_y;
-	double	dir_length;
-	int		end_x;
-	int		end_y;
-
-	center_x = MINIMAP_MARGIN + MINIMAP_SIZE / 2;
-	center_y = WINDOW_HEIGHT - MINIMAP_SIZE - MINIMAP_MARGIN + MINIMAP_SIZE / 2;
-	draw_player_square(game, center_x, center_y);
-	dir_length = MINIMAP_TILE_SIZE * 1.5;
-	end_x = center_x + (int)(game->player.dir[0] * dir_length);
-	end_y = center_y + (int)(game->player.dir[1] * dir_length);
-	draw_line(game, center_x, center_y, end_x, end_y, 0xFF4444);
-}
-
-static int	get_tile_color(char tile)
-{
-	int	color;
-
-	if (tile == '1')
-		color = 0x808080;
-	else if (tile == '0')
-		color = 0xE0E0E0;
-	else if (tile == ' ')
-		color = 0x404040;
-	else
-		color = 0x604040;
-	return (color);
-}
-
-static void	process_minimap_tile(t_data *game, double player_map_y,
-		int minimap_center_y, int minimap_x, int minimap_y, int map_x)
-{
-	int		map_y;
-	int		screen_x;
-	int		screen_y;
-	char	tile;
-	int		color;
-
-	map_y = (int)player_map_y + (minimap_y - minimap_center_y);
-	screen_x = MINIMAP_MARGIN + minimap_x * MINIMAP_TILE_SIZE;
-	screen_y = WINDOW_HEIGHT - MINIMAP_SIZE - MINIMAP_MARGIN + minimap_y
-		* MINIMAP_TILE_SIZE;
-	if (map_x >= 0 && map_x < game->map_width && map_y >= 0
-		&& map_y < game->map_height)
-	{
-		if (map_x < (int)ft_strlen(game->map[map_y]))
-		{
-			tile = game->map[map_y][map_x];
-			color = get_tile_color(tile);
-			draw_minimap_tile(game, screen_x, screen_y, color);
-		}
-	}
-}
-
-static void	render_minimap_tiles(t_data *game, double player_map_x,
-		double player_map_y)
-{
-	int	minimap_center_x;
-	int	minimap_center_y;
-	int	minimap_x;
-	int	minimap_y;
-	int	map_x;
-
-	minimap_center_x = MINIMAP_SIZE / 2 / MINIMAP_TILE_SIZE;
-	minimap_center_y = MINIMAP_SIZE / 2 / MINIMAP_TILE_SIZE;
-	minimap_y = 0;
-	while (minimap_y < MINIMAP_SIZE / MINIMAP_TILE_SIZE)
-	{
-		minimap_x = 0;
-		while (minimap_x < MINIMAP_SIZE / MINIMAP_TILE_SIZE)
-		{
-			map_x = (int)player_map_x + (minimap_x - minimap_center_x);
-			process_minimap_tile(game, player_map_y, minimap_center_y,
-				minimap_x, minimap_y, map_x);
-			minimap_x++;
-		}
-		minimap_y++;
-	}
+    draw_test_directions(game, center.x, center.y);
 }
 
 void	render_minimap(t_data *game)
 {
-	double player_map_x;
-	double player_map_y;
-
-	player_map_x = game->player.pos[0] / TILESIZE;
-	player_map_y = game->player.pos[1] / TILESIZE;
-	draw_minimap_background(game);
-	render_minimap_tiles(game, player_map_x, player_map_y);
-	draw_minimap_player(game);
+	draw_modern_minimap_background(game);
+	render_precise_tiles(game);
+	draw_modern_player(game);
 }
--- src/parse_texture.c
+++ src/parse_texture.c
@@ -3,114 +3,64 @@
 /*                                                        :::      ::::::::   */
 /*   parse_texture.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lsadi <marvin@42.fr>                       +#+  +:+       +#+        */
+/*   By: reeer-aa <reeer-aa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/24 05:19:58 by lsadi             #+#    #+#             */
-/*   Updated: 2025/07/24 05:19:59 by lsadi            ###   ########.fr       */
+/*   Updated: 2025/08/07 11:52:32 by reeer-aa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-int is_texture_line(char *line)
+static int	assign_north_south_texture(t_config *config, char *path,
+		char *direction)
 {
-    char *trimmed;
-    int result;
-    
-    if (!line)
-        return (0);
-    
-    trimmed = ft_strtrim(line, " \t\n");
-    if (!trimmed)
-        return (0);
-    
-    result = 0;
-    if (ft_strncmp(trimmed, "NO ", 3) == 0)
-        result = 1;
-    else if (ft_strncmp(trimmed, "SO ", 3) == 0)
-        result = 1;
-    else if (ft_strncmp(trimmed, "WE ", 3) == 0)
-        result = 1;
-    else if (ft_strncmp(trimmed, "EA ", 3) == 0)
-        result = 1;
-    
-    free(trimmed);
-    return (result);
+	if (ft_strncmp(direction, "NO", 2) == 0)
+	{
+		if (config->texture_north)
+			return (printf("Error: Duplicate North texture\n"), 0);
+		config->texture_north = path;
+		return (1);
+	}
+	else if (ft_strncmp(direction, "SO", 2) == 0)
+	{
+		if (config->texture_south)
+			return (printf("Error: Duplicate South texture\n"), 0);
+		config->texture_south = path;
+		return (1);
+	}
+	return (-1);
 }
 
-int parse_texture_line(char *line, t_config *config)
+static int	assign_west_east_texture(t_config *config, char *path,
+		char *direction)
 {
-    char *path;
-    char *trimmed;
-
-    trimmed = ft_strtrim(line, " \t\n");
-    if (!trimmed)
-        return (0);
-
-    path = extract_texture_path(trimmed);
-    if (!path)
-    {
-        free(trimmed);
-        return (0);
-    }
-
-    if (!assign_texture_by_direction(config, path, trimmed))
-    {
-        free(path);
-        free(trimmed);
-        return (0);
-    }
-
-    free(trimmed);
-    return (1);
+	if (ft_strncmp(direction, "WE", 2) == 0)
+	{
+		if (config->texture_west)
+			return (printf("Error: Duplicate West texture\n"), 0);
+		config->texture_west = path;
+		return (1);
+	}
+	else if (ft_strncmp(direction, "EA", 2) == 0)
+	{
+		if (config->texture_est)
+			return (printf("Error: Duplicate East texture\n"), 0);
+		config->texture_est = path;
+		return (1);
+	}
+	return (-1);
 }
 
-char *extract_texture_path(char *trimmed)
+int	assign_texture_by_direction(t_config *config, char *path, char *direction)
 {
-    char *path;
+	int	result;
 
-    path = ft_strchr(trimmed, ' ');
-    if (!path)
-    {
-        printf("Erreur : ligne de texture invalide.\n");
-        return (NULL);
-    }
-    path += 1;
-    return (ft_strtrim(path, " \t\n"));
-}
-
-int assign_texture(char *trimmed, char *path, t_config *config)
-{
-    return (assign_texture_by_direction(config, path, trimmed));
-}
-
-int assign_texture_by_direction(t_config *config, char *path, char *direction)
-{
-    if (ft_strncmp(direction, "NO", 2) == 0)
-    {
-        if (config->texture_north)
-            return (printf("Error: Duplicate North texture\n"), 0);
-        config->texture_north = path;
-    }
-    else if (ft_strncmp(direction, "SO", 2) == 0)
-    {
-        if (config->texture_south)
-            return (printf("Error: Duplicate South texture\n"), 0);
-        config->texture_south = path;
-    }
-    else if (ft_strncmp(direction, "WE", 2) == 0)
-    {
-        if (config->texture_west)
-            return (printf("Error: Duplicate West texture\n"), 0);
-        config->texture_west = path;
-    }
-    else if (ft_strncmp(direction, "EA", 2) == 0)
-    {
-        if (config->texture_est)
-            return (printf("Error: Duplicate East texture\n"), 0);
-        config->texture_est = path;
-    }
-    else
-        return (0);
-    return (1);
+	result = assign_north_south_texture(config, path, direction);
+	if (result != -1)
+		return (result);
+	result = assign_west_east_texture(config, path, direction);
+	if (result != -1)
+		return (result);
+	return (0);
 }
